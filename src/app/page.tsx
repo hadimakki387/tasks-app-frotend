4198436b0195b2661bc51823dffe713d8fad8792
hadimakki387 <hmakki387@gmail.com>
2023-08-13 13:10:00 +0300

    wraped the app with the provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,15 @@
 import App from '@/components/home/App'
 import { ApiProvider } from "@reduxjs/toolkit/query/react";
 import { homeApi } from './api/apiSlice';
-
+import { store } from "@/app/store";
+import { Provider } from "react-redux";
 
 export default function Home() {
   return (
     <ApiProvider api={homeApi}>
-      <App/>
+      <Provider store={store}>
+        <App/>
+      </Provider>
     </ApiProvider>
       
   )
