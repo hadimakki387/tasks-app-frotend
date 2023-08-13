67b9a21312630ef4d4c265d0f76ee78a5b345cca
hadimakki387 <hmakki387@gmail.com>
2023-08-13 13:08:19 +0300

    added the slice and the api to the store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
-
+import tasksReducer from './slices/tasksSlice';
 import { setupListeners } from '@reduxjs/toolkit/query/react'; // Import the setupListeners function
+import { homeApi } from './api/apiSlice';
 
 
 export const store = configureStore({
   reducer: {
-  
+    tasks: tasksReducer, // Add the tasks reducer
+    [homeApi.reducerPath]: homeApi.reducer,
   },
-
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(homeApi.middleware), // Add RTK-Query middleware
 });
 
 setupListeners(store.dispatch); // Attach the setupListeners function to the store
