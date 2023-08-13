ed8046e62be27dd863e08ecb6b6eb508f4c356d8
hadimakki387 <hmakki387@gmail.com>
2023-08-13 12:52:57 +0300

    sent data to the server to be saved

--- src/app/api/apiSlice.ts
+++ src/app/api/apiSlice.ts
@@ -4,19 +4,19 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const homeApi = createApi({
   reducerPath: "homeApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8080/api/" }), // Adjust the baseUrl as needed
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:4000/api/" }), // Adjust the baseUrl as needed
   endpoints: (builder) => ({
     getHomeData: builder.query({
       query: ({page,limit,cat}) => `home?page=${page}&limit=${limit}&cat=${cat}`, // This should match the route on your server
     }),
     addTask: builder.mutation({
-        query: ({task,userId}) => ({
+        query: (task) => ({
           url: `add-task`,
           headers: {
             "Content-Type": "application/json",
           },
           method: "POST",
-          body: {task,userId},
+          body: task,
         }),
       }),
   }),
